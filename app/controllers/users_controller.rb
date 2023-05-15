8588e2a95b22e801a86f59623df1da22a61d2115
Seiji Kakogawa <sayzkkgw@gmail.com>
2023-05-15 03:28:40 +0000

    Implement users resources and Gravar component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
-  def new
+  def index; end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # debugger
+
+    # => /app/views/users/show.html.erb
   end
+
+  # GET /users/new
+  def new
+  
+    # => /app/views/users/new.html.erb
+  end
+
 end
