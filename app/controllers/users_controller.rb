84a3bf474df810497e09ca91bce6f199b0907fb3
Seiji Kakogawa <sayzkkgw@gmail.com>
2023-05-15 13:57:24 +0000

    Add error shard, form improved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,24 @@ class UsersController < ApplicationController
     # => /app/views/users/show.html.erb
   end
 
-  # GET /users/new
+  # GET /users/new or /signup
   def new
-  
+    @user = User.new
     # => /app/views/users/new.html.erb
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Success Process
+    else
+      render 'new' #, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
