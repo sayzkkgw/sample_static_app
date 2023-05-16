fb09915bfe63e76b341c3a64fa56b00eb018d35a
Seiji Kakogawa <sayzkkgw@gmail.com>
2023-05-16 05:11:16 +0000

    Add flush and integration test for signup success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Success Process
+      flash[:success] = 'Welcome to the sample app!'
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
