1361b20a531e4510850456a1c0df79e93281b326
Seiji Kakogawa <sayzkkgw@gmail.com>
2023-05-27 15:47:43 +0000

    rollback sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails db:migrate:reset
+# bundle exec rails db:seed
 set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+bundle exec rails db:migrate
